c0dfafacdd98bdc0b0ebbfb0e2c3cdf2a00d839d
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2012-11-22 23:44:27 +0530

    A lot of changes to design and base data structures.
    The initial design was flawed. This new design assumes that a rpc
    server is running on the supplied workers and uses these services for
    communication.

--- mapred/mapred.go
+++ mapred/mapred.go
@@ -4,60 +4,43 @@ package mapred
 import (
 	"fmt"
 	"net"
+	"net/rpc"
+	"os"
 )
 
-// Defines wether a worker is a mapworker, reduce worker or the master
-const (
-	mapWorker = iota
-	reduceWorker
-	master
-)
+// State of a worker
+type state int
 
 // State of a host
 const (
-	idle = iota
+	idle state = iota
 	inProgress
 	completed
 	failed
 )
 
-// Renaming the generic type - Id from objective-c
-type Id interface{}
+// default chunk size is 64MB
+const defaultChunkSize uint64 = 1 << 14
 
-// Give input as a map instead of indivIdual key-value pairs to reduce the overhead of function calls
+// Renaming the generic type - id from objective-c
+type id interface{}
+
+// Give input as a map instead of individual key-value pairs to reduce the overhead of function calls
 // and accomodate as many key-value pairs as possible in a single function call. How many? That's left
-// to the user to decIde
-type MapInput map[Id]Id
+// to the user to decide
+type MapInput map[id]id
 
-// Result of the mapreduce computations which includes the result
-type MapReduceResult struct {
-	Result map[Id]Id
-}
+// Result of the mapreduce result - same as MapInput
+type MapReduceResult MapInput
 
 // Intermediate key-value pairs that are emitted
 type Intermediate struct {
-	Key, Value Id
+	Key, Value id
 }
 
 // We use a channel to be able to iterate over the values as and when they are available. This allows
 // very large lists to be handled since only as many items are sent through the channel as can fit in memory.
-type ReduceInput map[Id]chan Id
-
-// The map function to be provIded by the user
-type MapFunc func(in <-chan MapInput, emit chan<- Intermediate) error
-
-// The Reader should read in bytes of chunksize and returns a channel of MapInput
-type Reader interface {
-	Read(chunk []byte) (chan MapInput, error)
-}
-
-// The Writer should read in bytes of chunksize and returns a channel of ReduceInput
-type Writer interface {
-	Write(chunk []byte) error
-}
-
-// the reduce function - provIded by the user
-type ReduceFunc func(in <-chan ReduceInput, out <-chan Id) error
+type ReduceInput map[id]chan id
 
 // the mapreduce specification object
 type Specs struct {
@@ -65,51 +48,50 @@ type Specs struct {
 	InputFiles, OutputFiles []string
 	// Total number of mappers and reducers to use
 	M, R uint
-	// Chunk of data to be sent to each host.
-	// This should determined on the basis of the type of the underlying filesystem and is unexported
+	// Size of each split.
+	// This should determined on the basis of the type of the underlying filesystem.
 	ChunkSize uint64
-	/* The workers can either be supplied as a slice of Worker structs or as a network address on which a Worker equest
-		is broadcasted. Workers which reply in the affirmative are assigned. Setting both a network address as well as a host
-	    slice containing one or more Workers is an error!
-	*/
+	// The workers can either be supplied as a slice of worker structs or as a network address on which a "Worker.Idle" request
+	// is broadcasted. Sorkers which reply in the affirmative are assigned. Setting both a network address as well as a host
+	// slice containing one or more workers is an error.
 	Network string
-	// The length of this slice is >= M+R
-	Workers []Worker
-
-	// Functions
-	inputReader Reader
-	mapFunc     MapFunc
-	reduceFunc  ReduceFunc
+	// The length of this slice should be >= M+R
+	Workers []worker
 }
 
 // Represents a machine in the cluster
-type Worker struct {
+type worker struct {
 	// The IP address of the host
-	IPAddr net.Addr
-	// Port on which the host is listening
-	Port uint16
-	// Tasks assigned to the host
-	tasks []Task
+	Addr net.TCPAddr
+	// Task assigned to the worker
+	task id
 }
 
-type Task struct {
-	// State of the host
-	state int
+// Task consists of the current state of the task, the split assigned to it if it is a map task
+type MapTask struct {
+	// State of the task
+	state state
+	// Split assigned to this task - valid only if it is a map task.
+	split os.File
+	// Same as R, used by map workers
+	r uint
 }
 
-// default chunk size is 64MB
-const defaultChunkSize = 1 << 16
+type ReduceTask struct {
+	// State of the task
+	state state
+}
 
 // The MapReduce() call that triggers off the magic
 func MapReduce(specs Specs) (result MapReduceResult, err error) {
-	// ValIdate the specs
-	if err = valIdateSpecsObject(specs); err != nil {
+	// Validate the specs
+	if err = validateSpecs(specs); err != nil {
 		return
 	}
 	if specs.Network != "" {
 		// Populate the hosts slice of specs based on the network
-		hostChan := make(chan Worker, specs.M+specs.R)
-		if err = populateHosts(specs, hostChan); err != nil {
+		var hostChan chan worker
+		if hostChan, err = populateHosts(specs); err != nil {
 			return
 		}
 		for w := range hostChan {
@@ -118,25 +100,68 @@ func MapReduce(specs Specs) (result MapReduceResult, err error) {
 		}
 	}
 
-	// Assign the work to all the hosts
+	// Determine the total size of the all input files and start splitting it. Assign each split to a worker as a map task
+	// and assign a reduce task to the rest of the workers in a ratio of M:R
+	totalWorkers := uint(len(specs.Workers))     // total workers
+	clients := make([]*rpc.Client, totalWorkers) // clients returned by rpc calls
+	calls := make([]*rpc.Call, totalWorkers)     // calls returned by rpc calls
+	unit := totalWorkers / (specs.M + specs.R)   // unit worker for ratios
+	ans := make([]*bool, totalWorkers)
+	m := int(unit * specs.M) // number of map workers
+	r := int(unit * specs.R) // number of reduce workers
+	dialFailures := 0        // number of dial failure - max limit is
+	myAddr := myTCPAddr()
+	for i := 0; i < m+r; i++ {
+		clients[i], err = rpc.DialHTTP("tcp", specs.Workers[i].Addr.String())
+		if err != nil {
+			dialFailures++
+			if dialFailures > m/2 {
+				err = fmt.Errorf("Number of dial failures is more than %d", m/2)
+				return // Return if number of dial failures are too many
+			}
+		}
+		calls[i] = clients[i].Go(idleService, myAddr, ans[i], nil) // Call the service method to ask if the host is idle
+	}
+
+	err = nil // Reset err
 	return
 }
 
-func valIdateSpecsObject(specs Specs) error {
+func validateSpecs(specs Specs) error {
 	// Only one of them should be set
-	validNetwork := specs.Network != ""
-	validWorkers := specs.Workers != nil
-	if !((validNetwork && !validWorkers) || (!validNetwork && validWorkers)) {
+	validNetwork := bToi(specs.Network != "")
+	validWorkers := bToi(specs.Workers != nil)
+	if validNetwork^validWorkers != 0 {
 		return fmt.Errorf("Specs object should have either a network address: %s or a slice of hosts: %v, not both!", specs.Network, specs.Workers)
 	}
-	// If []Workers is set then M+R >= num of Workers
-	if validWorkers && specs.M+specs.R < uint(len(specs.Workers)) {
+	// If []workers is set then M+R >= num of workers
+	if validWorkers != 0 && specs.M+specs.R < uint(len(specs.Workers)) {
 		return fmt.Errorf("M: %d & R: %d are less than the num of hosts: %d", specs.M, specs.R, len(specs.Workers))
+	}
+	// If the chunk size specified is less than 16Mb then set it to 16Mb
+	if specs.ChunkSize < defaultChunkSize {
+		specs.ChunkSize = defaultChunkSize
 	}
 	return nil
 }
 
-func populateHosts(specs Specs, hostChan chan Worker) error {
-	// Populate Workers from specs.Network until the num of workers is >= M+R
-	return nil
+func populateHosts(specs Specs) (hostChan chan worker, err error) {
+	// Populate workers from specs.Network by broadcasting an "Worker.Idle" message until the num of workers is >= M+R
+	// TODO implement later
+	// hostChan := make(chan worker, specs.M+specs.R)
+	err = fmt.Errorf("Providing Network is not supported by the package right now")
+	return
+}
+
+// Converts a bool to int
+func bToi(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func myTCPAddr() net.TCPAddr {
+	// Return own ip address used for the connections
+	return net.TCPAddr{}
 }
--- mapred/worker.go
+++ mapred/worker.go
@@ -2,39 +2,66 @@
 package mapred
 
 import (
+	"errors"
+	"net"
+	"net/http"
 	"net/rpc"
 )
 
-var (
-	started bool // The staretd?
+const (
+	port        = ":2607" // All communication happens over this port
+	idleService = "service.Idle"
 )
 
-// The object that will be exposed as a service for map work
-type Mapper struct{}
+var (
+	started bool         // Indicates wether the service has staretd
+	l       net.Listener // The service listener
+	master  net.TCPAddr  // Address of the master
+	task    id           // Current task
+)
 
-// The object that will be exposed as a service for reduce work
-type Reducer struct{}
-
-// Arguments for a map work
-type MapArgs struct {
-	in      <-chan MapInput
-	emit    chan<- Intermediate
-	mapFunc MapFunc
+// Worker is an inteface that defines the behaviour required by the application.
+// It is important that these methods are defined on a pointer for the rpc mechanism to work.
+// A Worker object is passed to Start() to start providing the service
+type Worker interface {
+	// Read takes as input a byte slice, converts it into a MapInput and returns it back
+	Read([]byte) (MapInput, error)
+	// Map is the map function which takes a channel of MapInput and emits
+	// the Intermediate key-value pairs for every key-value pair of MapInput through the channel
+	// returned by it.
+	Map(<-chan MapInput) (<-chan Intermediate, error)
+	//Reduce is the reduce function which takes a channel of ReduceInput and an output channel to transfer
+	Reduce(<-chan ReduceInput) (<-chan id, error)
 }
 
-// Arguments for a reduce work
-type ReduceArgs struct {
-	in      <-chan ReduceInput
-	out     <-chan Id
-	redFunc ReduceFunc
+type service struct{}
+
+// Asks wether the host is willing to work on a mapreduce task. It replies yes only if it is idle.
+// The first argument is the tcp address of the master. The second arguement contains the reply.
+func (w *service) Idle(ip net.TCPAddr, reply *bool) error {
+	*reply = task == nil
+	return nil
 }
 
-func (m *Mapper) DoMapWork(args MapArgs, err *error) error {
-
+func Start(worker *Worker) error {
+	// Check if the service is already running or not
+	if started {
+		return errors.New("Service has already been started. Use Stop() to stop the service.")
+	}
+	rpc.Register(worker)
+	rpc.Register(&service{})
+	rpc.HandleHTTP()
+	var e error
+	l, e = net.Listen("tcp", port)
+	if e != nil {
+		return e
+	}
+	go http.Serve(l, nil)
+	started = true
+	return nil
 }
 
-func Start() {
-	mapper := new(Mapper)
-	reducer := new(Reducer)
-	rpc.Register(mapper)
+func Stop() error {
+	started = false
+	return l.Close()
 }
